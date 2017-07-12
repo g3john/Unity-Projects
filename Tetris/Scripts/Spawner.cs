ef07ec0beadc06c25caf9f8c1158fbec34e69678
John Lacuna <john.lacuna@mail.utoronto.ca>
2017-07-12 02:49:36 -0400

    Add files via upload

---
+++ Tetris/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] blocks; 
+	// Use this for initialization
+	void Start () {
+        spawnNext();
+	}
+	
+	public void spawnNext()
+    {
+        int i = Random.Range(0, blocks.Length);
+
+        Instantiate(blocks[i],
+            transform.position,
+            Quaternion.identity);
+    }
+}
